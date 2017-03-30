353294063f3199dc84f925addb8bc37c3a02c2bf
Kaveesh <kbaddage98@gmail.com>
2017-03-30 22:50:20 +0530

    ng-model

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,12 +1,18 @@
  var app = angular.module('myapp',[]);
  	
  	app.controller('meetupsController',function($scope){
- 		$scope.meetupsCount = 10;
-
+ 		
  		$scope.meetups = [
  			{name : "1 meet"},
  			{name : "2 meet"},
  			{name : "3 meet"}
  		]
- 	});
 
+ 		$scope.createMeetup = function(){
+ 		$scope.meetups.push({name:$scope.meetupName});
+ 		$scope.meetupName = '';
+ 	    }
+
+});
+
+ 	
