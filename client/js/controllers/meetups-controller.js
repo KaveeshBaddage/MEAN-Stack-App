72a91743355ccd2e323c07469caec464926d7e4e
Kaveesh <kbaddage98@gmail.com>
2017-03-30 22:12:42 +0530

    ng-repeatFunction

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -2,5 +2,11 @@
  	
  	app.controller('meetupsController',function($scope){
  		$scope.meetupsCount = 10;
+
+ 		$scope.meetups = [
+ 			{name : "1 meet"},
+ 			{name : "2 meet"},
+ 			{name : "3 meet"}
+ 		]
  	});
 
